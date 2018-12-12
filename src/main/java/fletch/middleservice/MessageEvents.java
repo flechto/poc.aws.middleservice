7fa9f5a8131bcc694fd153c89da569f8d8e6b701
fletch <flechto@gmail.com>
2018-12-11 23:10:53 -0600

    send message to sns when creating

--- src/main/java/fletch/middleservice/MessageEvents.java
+++ src/main/java/fletch/middleservice/MessageEvents.java
@@ -1,10 +1,24 @@
 package fletch.middleservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageEvents {
-    public void emit(Message message) {
 
+    private final NotificationMessagingTemplate snsTemplate;
+
+    @Autowired
+    public MessageEvents(NotificationMessagingTemplate snsTemplate) {
+        this.snsTemplate = snsTemplate;
+    }
+
+    public void emit(Message message) {
+//        snsTemplate.sendNotification(message, "created");
+        snsTemplate.sendNotification(
+                "created",
+                message,
+                "created");
     }
 }
--- src/main/java/fletch/middleservice/MiddleServiceApplication.java
+++ src/main/java/fletch/middleservice/MiddleServiceApplication.java
@@ -1,17 +1,28 @@
 package fletch.middleservice;
 
+import com.amazonaws.services.sns.AmazonSNS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.messaging.config.annotation.EnableSns;
 import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
+import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableSqs
+@EnableSns
 public class MiddleServiceApplication {
 
 	public static void main(String[] args) {
 
 	    SpringApplication.run(MiddleServiceApplication.class, args);
+	}
+
+	@Bean
+	public NotificationMessagingTemplate snsTemplate(AmazonSNS amazonSNS) {
+
+		return new NotificationMessagingTemplate(amazonSNS);
 	}
 }
